7e77f362dbb81a04ee5d767ccfbf3a73e3e70ea2
Priscilla Ashley Wijaya <priscilla.a.wijaya@gmail.com>
2026-05-30 21:47:04 +0800

    fix mobile layout on deployment

--- frontend/src/components/layout/MobileLayout.tsx
+++ frontend/src/components/layout/MobileLayout.tsx
@@ -11,9 +11,9 @@ export default function MobileLayout({
     showChrome?: boolean;
 }) {
     return (
-        <div className="bg-gray-100 min-h-screen flex justify-center items-center sm:py-5">
+        <div className="bg-gray-100 min-h-screen flex justify-center items-center md:py-5">
             <div className="bg-[#faf8f5] flex flex-col relative overflow-hidden w-full h-screen
-                            sm:w-[360px] sm:h-[740px] sm:rounded-[40px] sm:border-8 sm:border-CropLink-dark sm:shadow-2xl">
+                            md:w-[360px] md:h-[740px] md:rounded-[40px] md:border-8 md:border-CropLink-dark md:shadow-2xl">
                 {showChrome && <Header />}
                     <div className="flex-1 overflow-y-auto no-scrollbar">
                         {children}
